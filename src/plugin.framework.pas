b580bba31f8fb4f3bf1dd1952d654051ac73ef1d
Jacco Uijlenhoet <91184300+OwlHatSoftware@users.noreply.github.com>
2025-05-15 17:21:12 +0200

    First commit

---
+++ src/plugin.framework.pas
@@ -0,0 +1,65 @@
+unit plugin.framework;
+
+interface
+
+uses
+  Winapi.Windows,
+  Winapi.Messages,
+  System.SysUtils,
+  System.Variants,
+  System.Classes;
+
+type
+  TPlugInFrameWork = class
+  private
+  class var LibHandle: THandle;
+  public
+    class function LoadPlugin<I: IInterface>(const ALibName,
+      AFuncName: string): I;
+    class procedure FreePlugIn(const AFuncName: string);
+  end;
+
+implementation
+
+{ TPlugInFrameWork }
+
+class procedure TPlugInFrameWork.FreePlugIn(const AFuncName: string);
+type
+  TFreePluginProc = procedure; stdcall;
+var
+  FreePluginProc: TFreePluginProc;
+begin
+  if LibHandle <> 0 then
+  begin
+    @FreePluginProc := GetProcAddress(HMODULE(LibHandle), PChar(AFuncName));
+    if Assigned(FreePluginProc) then
+    begin
+      FreePluginProc;
+      Exit; // don't free the library, the interface is released automatically!
+    end;
+  end;
+end;
+
+class function TPlugInFrameWork.LoadPlugin<I>(const ALibName,
+  AFuncName: string): I;
+type
+  TGetPluginFunc = function: I; stdcall;
+var
+  GetPluginFunc: TGetPluginFunc;
+begin
+  LibHandle := LoadLibrary(PChar(ALibName));
+  if LibHandle <> 0 then
+  begin
+    @GetPluginFunc := GetProcAddress(HMODULE(LibHandle), PChar(AFuncName));
+    if Assigned(GetPluginFunc) then
+    begin
+      result := GetPluginFunc;
+      Exit; // don't free the library, the interface is released automatically!
+    end;
+    FreeLibrary(LibHandle); // only free the library if the plugin wasn't found!
+  end
+  else
+    result := nil;
+end;
+
+end.
